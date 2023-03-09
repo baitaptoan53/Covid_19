02a57d5427c123f15470b892258f67fc0b144352
Nguyễn Xuân Ngọc <84265122+baitaptoan53@users.noreply.github.com>
2023-03-09 23:22:56 +0700

    home page v1

---
+++ app/src/main/java/com/example/covid_19/ui/home/HomeViewModel.java
@@ -0,0 +1,16 @@
+package com.example.covid_19.ui.home;
+
+
+import android.annotation.SuppressLint;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+
+public abstract class HomeViewModel extends ViewModel {
+
+}
